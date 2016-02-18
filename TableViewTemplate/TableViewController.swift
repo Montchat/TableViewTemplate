c3f65139983a10242ede1bd905d1382197e4d8e8
Montchat <je4440@gmail.com>
2016-02-17 20:55:40 -0500

    updated editting features

--- TableViewTemplate/TableViewController.swift
+++ TableViewTemplate/TableViewController.swift
@@ -81,8 +81,7 @@ class TableViewController: UITableViewController {
 //     Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
-            
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            objectArray[indexPath.section].sectionObjects.removeAtIndex(indexPath.row) ; tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
             
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
